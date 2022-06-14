cb1af026a9c77473a49843c03064b315b9ea166e
kirti gaurav mishra <kirtigauravmishra@gmail.com>
2022-06-14 15:25:28 +0530

    fixed major bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,47 @@ import React,{useEffect,useState} from 'react';
 import Navbar from './Components/Navbar';
 import Video from './Pages/Video';
 import './aapi.css';
+
 import Data from './data';
 
+
+
+
 //bara (Video) = Data
+// we will pass the data of our videos here 
 const App = () => {
- 
+ const [ytvideos, setytvideos]= useState([]);
+
+
+useEffect(()=>{
+  setytvideos(Video);
+}, []);
+
 
   return (
+
+
 
    <div> <Navbar />
 
     <div className='app'>
-        <div className='app_videos'>   <Video/> </div>
+        <div className='app_videos'>   <Video/>
+
+        {Data.map((vid) => (
+          <Video
+            id={vid.id}
+            src={vid.url}
+          />
+        ))}
+          
+        
+        
+         </div>
       
-     
     </div>
     </div>
   
-  )
+  );
 }
 
 export default App;
--- src/data.js
+++ src/data.js
@@ -1,20 +1,34 @@
 import React from "react";
 
 const Data = [
-    {
-      id: 1,
-      url:
-        " https://rr4---sn-1gi7znek.googlevideo.com/videoplayback?expire=1651232641&ei=IXtrYtuBJLDIx_APx6aHyAo&ip=154.95.32.234&id=o-AMHDHnwTBj6YFrmpNGa14cKuprD98smsP4iob3_ZujTk&itag=18&source=youtube&requiressl=yes&mh=7R&mm=31%2C26&mn=sn-1gi7znek%2Csn-aigl6ner&ms=au%2Conr&mv=m&mvi=4&pl=24&pcm2=no&initcwndbps=273750&vprv=1&mime=video%2Fmp4&gir=yes&clen=1626802&ratebypass=yes&dur=15.061&lmt=1646210210803700&mt=1651210868&fvip=1&fexp=24001373%2C24007246&c=ANDROID&txp=6310224&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cpcm2%2Cvprv%2Cmime%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRgIhAIdRO7xMabAkKZNe9VLOtnH9Dt2NNPO-ti7TFDFjRS8VAiEA0g5DkPSWJQMI68mfYUVvx5JjPTEfExoGFF5V-3sOlq8%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRQIhAJhPFP-QCPulTpahhICvwM3-bEvOKRiTnsiYk-VUk_sVAiAv6yjVWlp2iQt2Hp5X_IVXyfprm97ZXPI60M6uZ4253w%3D%3D"},
+
+  {
+    id: 1,
+    url: "https://img23.ropose.com/video/mvid/bISCoHc/uBKy_315560961836500fdd44d11-be16-474e-aac5-620df3d3e96a_4275937a_a822.mp4",
+  },
     {
       id: 2,
+      url:
+        " https://rr4---sn-1gi7znek.googlevideo.com/videoplayback?expire=1651232641&ei=IXtrYtuBJLDIx_APx6aHyAo&ip=154.95.32.234&id=o-AMHDHnwTBj6YFrmpNGa14cKuprD98smsP4iob3_ZujTk&itag=18&source=youtube&requiressl=yes&mh=7R&mm=31%2C26&mn=sn-1gi7znek%2Csn-aigl6ner&ms=au%2Conr&mv=m&mvi=4&pl=24&pcm2=no&initcwndbps=273750&vprv=1&mime=video%2Fmp4&gir=yes&clen=1626802&ratebypass=yes&dur=15.061&lmt=1646210210803700&mt=1651210868&fvip=1&fexp=24001373%2C24007246&c=ANDROID&txp=6310224&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cpcm2%2Cvprv%2Cmime%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRgIhAIdRO7xMabAkKZNe9VLOtnH9Dt2NNPO-ti7TFDFjRS8VAiEA0g5DkPSWJQMI68mfYUVvx5JjPTEfExoGFF5V-3sOlq8%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRQIhAJhPFP-QCPulTpahhICvwM3-bEvOKRiTnsiYk-VUk_sVAiAv6yjVWlp2iQt2Hp5X_IVXyfprm97ZXPI60M6uZ4253w%3D%3D",
+    },
+    {
+      id: 3,
       url:
         "https://rr1---sn-5hnekn7z.googlevideo.com/videoplayback?expire=1651108095&ei=n5RpYom1AqKC6dsP4fGOwA0&ip=195.158.192.240&id=o-AP8_WQ56-4GTyvwmJvE9TLgnQ6vCDHl9IFFJFWOhehkl&itag=18&source=youtube&requiressl=yes&mh=Vl&mm=31%2C26&mn=sn-5hnekn7z%2Csn-aigzrnld&ms=au%2Conr&mv=m&mvi=1&pl=22&initcwndbps=691250&vprv=1&mime=video%2Fmp4&gir=yes&clen=586466&ratebypass=yes&dur=11.114&lmt=1651032302780159&mt=1651086052&fvip=3&fexp=24001373%2C24007246&c=ANDROID&txp=6310224&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRgIhANFMfcEpaxPJo7AwLeolz0rCIwaU41f5xAh25XtfCHwuAiEA52knxLaRd2EQt2wQ-NhX1Tz01Bn_vKTQieGm65T1eCQ%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRQIgd4JA_ypPbOLGWqtKUc5OAWY1yHBzuobgZpM7oVcAN28CIQD8_EzC0IGQiJTcD5OnXQCT2uCC3XXz4kaq7--Nd_YQwA%3D%3D",
     },
 
     {
-        id:3,
+        id:4,
         url:"https://rr1---sn-5hne6nzk.googlevideo.com/videoplayback?expire=1651107900&ei=3JNpYpf2H6LIx_APv92j0AY&ip=193.8.231.70&id=o-ADQPCIhnp-0LSeBzfttvn5SPoZNlOnEpa-hijGu4AnTR&itag=18&source=youtube&requiressl=yes&mh=PG&mm=31%2C29&mn=sn-5hne6nzk%2Csn-5hnekn76&ms=au%2Crdu&mv=m&mvi=1&pl=24&pcm2=yes&initcwndbps=336250&vprv=1&mime=video%2Fmp4&gir=yes&clen=914252&ratebypass=yes&dur=17.237&lmt=1651071257581602&mt=1651085812&fvip=5&fexp=24001373%2C24007246&c=ANDROID&txp=6310224&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cpcm2%2Cvprv%2Cmime%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRgIhANH5V1gyIH5YnGl6klJv2h3psObBfOm21dJfbh-zh80-AiEAj6_qgahUctVwdqQiAMk_yA6Yh_cTYZVmcTIs3RSG0Ww%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRAIgKjFTvIsZlfQrw7KJ0AiUiBv5q1EDvNkfToIH9RRar7MCIATG-HV6rv889HTvhXymm-HVwC9uAfbC4gpUUnH8GXlj",
     },
+
+    {
+      id: 5,
+      url:
+        "https://img23.ropose.com/video/mvid/bISCoHc/uBKy_315560961836500fdd44d11-be16-474e-aac5-620df3d3e96a_4275937a_a822.mp4",
+    },
+
+    
   ];
   
   export default Data;
